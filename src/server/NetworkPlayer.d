48cc2c2fa3a5b098422c892e0330126bd0ad9828
deavmi <deavmi>
2020-04-08 18:06:22 +0200

    Added network code for getting next move.

--- src/server/NetworkPlayer.d
+++ src/server/NetworkPlayer.d
@@ -5,6 +5,8 @@ import std.socket;
 
 public class NetworkPlayer : Player
 {
+    private Socket clientConnection;
+
     this(Socket clientConnection, Color color)
     {
         super(color);
@@ -12,6 +14,16 @@ public class NetworkPlayer : Player
 
     override string getNextMove()
     {
-        return "TODO";
+        /* Protocol is 1 byte (size), then string of that length */
+        byte[1] sizeBuffer;
+        clientConnection.receive(sizeBuffer, SocketFlags.PEEK);
+
+        /* Read sizeBuffer many bytes */
+        byte[] moveBuffer;
+        moveBuffer.length+=sizeBuffer[0]+1;
+        clientConnection.receive(moveBuffer);
+        moveBuffer = moveBuffer[1..moveBuffer.length];
+
+        return cast(string)cast(char[])moveBuffer;
     }
 }
